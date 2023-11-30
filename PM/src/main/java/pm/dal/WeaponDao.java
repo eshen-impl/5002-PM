1d90bdd5e050b070b2a86b4ed528072c91705fdb
eshen-impl <shenyn96@163.com>
2023-11-30 09:53:28 -0800

    add equip/gear/weapon/eqbonus model/dao/test

---
+++ PM/src/main/java/pm/dal/WeaponDao.java
@@ -0,0 +1,130 @@
+package pm.dal;
+
+import pm.model.*;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+
+/**
+ * This is used to store {@link Weapon} into your MySQL instance and 
+ * retrieve {@link Weapon} from MySQL instance.
+ */
+
+public class WeaponDao extends EquippableDao {
+	// Single pattern: instantiation is limited to one object.
+	private static WeaponDao instance = null;
+
+	protected WeaponDao() {
+		super();
+	}
+
+	public static WeaponDao getInstance() {
+		if(instance == null) {
+			instance = new WeaponDao();
+		}
+		return instance;
+	}
+
+/**
+ * 1. public Weapon create(Weapon weapon)
+ */
+
+	public Weapon create(Weapon weapon) throws SQLException {
+		// Insert into the superclass table first.
+		Integer item_id = weapon.getItemID();
+		if (item_id == null) {
+			Equippable equippable = super.create(new Equippable(weapon.getItemName(),weapon.getMaxStackSize(),weapon.getVendorPrice(),
+	                weapon.getCanBeSold(),weapon.getItemLevel(),weapon.getSlotType(),weapon.getRequiredJobLevel()));
+	        item_id = equippable.getItemID();
+			weapon.setItemID(item_id);
+		}
+
+		String insertWeapon = "INSERT INTO Weapon(itemID,damageDone,autoAttack,attackDelay,associatedJob) VALUES(?,?,?,?,?);";
+		Connection connection = null;
+		PreparedStatement insertStmt = null;
+		try {
+			connection = connectionManager.getConnection();
+            insertStmt = connection.prepareStatement(insertWeapon);
+            insertStmt.setInt(1, item_id);
+            insertStmt.setDouble(2, weapon.getDamageDone());
+            insertStmt.setDouble(3, weapon.getAutoAttack());
+            insertStmt.setDouble(4, weapon.getAttackDelay());
+         // Check if associatedJob is not null before accessing properties
+            insertStmt.setInt(5, (weapon.getAssociatedJob() != null) ? weapon.getAssociatedJob().getJobID() : null);
+
+            insertStmt.executeUpdate();
+            return weapon;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if(connection != null) {
+				connection.close();
+			}
+			if(insertStmt != null) {
+				insertStmt.close();
+			}
+		}
+	}
+
+	/**
+	 * 2.public Weapon getWeaponByItemID(int itemID)   
+	 */
+
+	public Weapon getWeaponByItemID(Integer itemID) throws SQLException {
+	    String selectWeapon =
+	            "SELECT Weapon.itemID AS ItemID, itemName, maxStackSize, vendorPrice, canBeSold, itemLevel, slotType, requiredJobLevel, damageDone, autoAttack, attackDelay, associatedJob " +
+	            "FROM Weapon " +
+	            "INNER JOIN Equippable ON Weapon.itemID = Equippable.itemID " +
+	            "INNER JOIN Item ON Equippable.itemID = Item.itemID " +
+	            "WHERE Weapon.itemID = ?;";
+	    Connection connection = null;
+	    PreparedStatement selectStmt = null;
+	    ResultSet results = null;
+	    try {
+	        connection = connectionManager.getConnection();
+	        selectStmt = connection.prepareStatement(selectWeapon);
+	        selectStmt.setInt(1, itemID);
+	        results = selectStmt.executeQuery();
+	        if (results.next()) {
+	            Integer resultItemID = results.getInt("ItemID");
+	            String itemName = results.getString("itemName");
+	            Integer maxStackSize = results.getInt("maxStackSize");
+	            BigDecimal vendorPrice = results.getBigDecimal("vendorPrice");
+	            Boolean canBeSold = results.getBoolean("canBeSold");
+	            Integer itemLevel = results.getInt("itemLevel");
+	            Equippable.SlotType slotType = Equippable.SlotType.valueOf(results.getString("slotType"));
+	            Integer requiredJobLevel = results.getInt("requiredJobLevel");
+	            Double damageDone = results.getDouble("damageDone");
+	            Double autoAttack = results.getDouble("autoAttack");
+	            Double attackDelay = results.getDouble("attackDelay");
+	            Job associatedJob = JobDao.getInstance().getJobByJobID(results.getInt("associatedJob"));
+
+	            Weapon weapon = new Weapon(resultItemID, itemName, maxStackSize, vendorPrice, canBeSold,
+	                    itemLevel, slotType, requiredJobLevel, damageDone, autoAttack, attackDelay, associatedJob);
+	            return weapon;
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        throw e;
+	    } finally {
+	        if (connection != null) {
+	            connection.close();
+	        }
+	        if (selectStmt != null) {
+	            selectStmt.close();
+	        }
+	        if (results != null) {
+	            results.close();
+	        }
+	    }
+	    return null;
+	}
+}
---
+++ PM/src/main/java/pm/model/EquippableBonus.java
@@ -0,0 +1,62 @@
+package pm.model;
+
+
+public class EquippableBonus {
+	protected Item itemID;
+	protected Attribute attribute;
+	protected double bonusValue;
+
+	public enum Attribute {
+		maxHP, strength, dexterity, vitality, intelligence, mind, criticalHit, determination, directHitRate, defense,
+		magicDefense, attackPower, skillSpeed, attackMagicPotency, healingMagicPotency, spellSpeed, averageItemLevel,
+		tenacity, piety
+	}
+
+	public EquippableBonus(Item itemID, Attribute attribute, double bonusValue) {
+		this.itemID = itemID;
+		this.attribute = attribute;
+		this.bonusValue = bonusValue;
+	}
+
+	public EquippableBonus(Item itemID) {
+		this.itemID = itemID;
+	}
+
+	public EquippableBonus(Item itemID, Attribute attribute) {
+		this.itemID = itemID;
+		this.attribute = attribute;
+	}
+
+	public EquippableBonus(Attribute attribute) {
+		this.attribute = attribute;
+	}
+
+	public EquippableBonus(double bonusValue) {
+		this.bonusValue = bonusValue;
+	}
+
+	/** Getters and setters. */
+	public Item getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(Item itemID) {
+		this.itemID = itemID;
+	}
+
+	public Attribute getAttribute() {
+		return attribute;
+	}
+
+	public void setAttribute(Attribute attribute) {
+		this.attribute = attribute;
+	}
+
+	public double getBonusValue() {
+		return bonusValue;
+	}
+
+	public void setBonusValue(double bonusValue) {
+		this.bonusValue = bonusValue;
+	}		
+}
